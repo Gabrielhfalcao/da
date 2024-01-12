5e514dd74f3d05c6c7cf159fbdfd656444ad78a7
Gabriel.Falcao <gabrielfalchonorato18@gmail.com>
2024-01-11 22:04:27 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dep = new Department(1, "Books");
+		
+		Seller seller = new Seller(1, "Gabriel", "gabriel@gmail.com", new Date(), 3000.0, dep);
+		
 		System.out.println(dep);
-		System.out.println("teste");
+		System.out.println(seller);
 	}
 
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,15 +1,22 @@
 package model.entities;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private Integer id;
 	private String name;
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
-	
+
 	private Department departament;
+	
+	public Seller() {
+	}
 
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department departament) {
 		this.id = id;
@@ -63,8 +70,31 @@ public class Seller {
 	public Integer getId() {
 		return id;
 	}
-	
+
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", departament=" + departament + "]";
 	}
 }
