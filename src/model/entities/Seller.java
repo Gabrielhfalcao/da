fe08e11795eb8785cec00315df2779ff294127dd
Gabriel.Falcao <gabrielfalchonorato18@gmail.com>
2024-01-11 21:44:16 -0300

    Department entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,70 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Seller {
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	
+	private Department departament;
+
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department departament) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.departament = departament;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Department departament) {
+		this.departament = departament;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
